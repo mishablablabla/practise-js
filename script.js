a08ef060be2ef10a8eb893af3b7a714a52141570
Misha <mzuew2006@gmail.com>
2024-01-16 18:59:13 +0200

    logical operators №2

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const hambueger = true;
-const fries = true;
+// const hambueger = true;
+// const fries = true;
 
-if (hambueger && fries) {
+// if (hambueger && fries) {
+//     console.log('я крутой йоу')
+// }
+
+const hambueger = 2;
+const fries = 1;
+
+if (hambueger === 3 && fries) {
     console.log('я крутой йоу')
+} else {
+    console.log('все плоха, еды нема(')
 }
